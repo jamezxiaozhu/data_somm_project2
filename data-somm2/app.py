6f3f2a2697df07b42940083309cbda7ed1e21f8d
jamezxiaozhu <jamezxiaozhu@me.com>
2020-03-04 11:34:55 -0500

    added to app.py and js reference code from previous homeworks

--- data-somm2/app.py
+++ data-somm2/app.py
@@ -103,3 +103,38 @@ def samples(sample):
 
 if __name__ == "__main__":
     app.run()
+
+
+
+#################################################
+# Database Setup - json into mongodb, from scraper
+#################################################
+
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from scrape_mars import scrape
+
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_info")
+
+
+@app.route("/")
+def index():
+    # write a statement that finds all the items in the db and sets it to a variable
+    mars_facts = mongo.db.collection.find_one()
+    # render an index.html template and pass it the data you retrieved from the database
+    return render_template("index.html", mars_data = mars_facts)
+
+@app.route("/scrape")
+def scrape_app():
+    # scrape_output = {}
+    results = scrape()
+    mongo.db.collection.update({}, results, upsert=True)
+    # Redirect back to home page
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
